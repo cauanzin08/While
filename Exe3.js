9f9305a6dff6f5829303dde46f4dbe41b193c375
Cauã Tupinambá <cauatupinamba@gmail.com>
2025-04-11 10:50:45 -0300

    Exe3

---
+++ Exe3.js
@@ -0,0 +1,10 @@
+let soma = 0
+let contador = 1
+
+console.log("\nSomando até chegar ou passar de 40:")
+while(soma <= 40){
+    soma += contador
+    console.log(" adicionei " +  contador  + " Soma "  +  soma)
+    contador++
+}
+console.log ("Usei "  +  ( contador - 1 )  + " numeros! ")
